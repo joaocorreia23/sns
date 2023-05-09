b5d140522f81b7e3450cd61d1f7fa3af4db1e35d
Rui Cruz <ruicrux@hotmail.com>
2023-05-09 22:20:09 +0100

    BD + API
    - Exams
    - Vaccines

--- sns_bd/functions/health_unit/health_unit.sql
+++ sns_bd/functions/health_unit/health_unit.sql
@@ -221,89 +221,6 @@ $$ LANGUAGE plpgsql;
 --
 --
 -- Add Health Unit Doctor
-CREATE OR REPLACE FUNCTION get_health_unit(
-    IN id_health_unit_in BIGINT DEFAULT NULL,
-    IN hashed_id_in VARCHAR(255) DEFAULT NULL
-)
-RETURNS TABLE (
-    hashed_id VARCHAR(255),
-    name VARCHAR(255),
-    phone_number VARCHAR(255),
-    email VARCHAR(255),
-    type INTEGER,
-    tax_number INT,
-    id_address BIGINT,
-    door_number VARCHAR(255),
-    floor VARCHAR(255),
-    address VARCHAR(255),
-    zip_code VARCHAR(255),
-    county_name VARCHAR(255),
-    district_name VARCHAR(255),
-    id_country BIGINT,
-    country_name VARCHAR(255),
-    created_at TIMESTAMP
-) AS $$
-BEGIN
-    IF hashed_id_in IS NULL AND id_health_unit_in IS NULL THEN
-        RETURN QUERY SELECT 
-            hu.hashed_id, hu.name, hu.phone_number, hu.email, hu.type, hu.tax_number, hu.id_address,
-            ad.door_number, ad.floor,
-            zc.address, zc.zip_code,
-            c.county_name,
-            d.district_name,
-            cty.id_country, cty.country_name,
-            hu.created_at
-        FROM health_unit hu
-        LEFT JOIN address ad ON hu.id_address=ad.id_address
-        LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
-        LEFT JOIN county c ON zc.id_county=c.id_county
-        LEFT JOIN district d ON c.id_district=d.id_district
-        LEFT JOIN country cty ON d.id_country=cty.id_country;
-    ELSEIF hashed_id_in IS NULL THEN
-        RETURN QUERY SELECT 
-            hu.hashed_id, hu.name, hu.phone_number, hu.email, hu.type, hu.tax_number, hu.id_address,
-            ad.door_number, ad.floor,
-            zc.address, zc.zip_code,
-            c.county_name,
-            d.district_name,
-            cty.id_country, cty.country_name,
-            hu.created_at
-        FROM health_unit hu
-        LEFT JOIN address ad ON hu.id_address=ad.id_address
-        LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
-        LEFT JOIN county c ON zc.id_county=c.id_county
-        LEFT JOIN district d ON c.id_district=d.id_district
-        LEFT JOIN country cty ON d.id_country=cty.id_country
-        WHERE hu.id_health_unit = id_health_unit_in;
-        IF NOT FOUND THEN
-            RAISE EXCEPTION 'Unidade de Saúde com o id "%" não existe', id_health_unit_in; --USER NOT FOUND
-        END IF;
-    ELSEIF id_health_unit_in IS NULL THEN
-       RETURN QUERY SELECT 
-            hu.hashed_id, hu.name, hu.phone_number, hu.email, hu.type, hu.tax_number, hu.id_address,
-            ad.door_number, ad.floor,
-            zc.address, zc.zip_code,
-            c.county_name,
-            d.district_name,
-            cty.id_country, cty.country_name,
-            hu.created_at
-        FROM health_unit hu
-        LEFT JOIN address ad ON hu.id_address=ad.id_address
-        LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
-        LEFT JOIN county c ON zc.id_county=c.id_county
-        LEFT JOIN district d ON c.id_district=d.id_district
-        LEFT JOIN country cty ON d.id_country=cty.id_country
-        WHERE hu.hashed_id = hashed_id_in;
-        IF NOT FOUND THEN
-            RAISE EXCEPTION 'Unidade de Saúde com o hashed_id "%" não existe', hashed_id_in; --USER NOT FOUND
-        END IF;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
---
---
---
--- Add Health Unit Doctor
 CREATE OR REPLACE FUNCTION add_health_unit_doctor(
     id_health_unit INTEGER,
     hashed_id_health_unit VARCHAR(255),
@@ -440,6 +357,8 @@ CREATE OR REPLACE FUNCTION get_health_unit_doctors(
 RETURNS TABLE(
     hashed_id_doctor VARCHAR(255), 
     doctor_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(255),
     link_date TIMESTAMP
 ) AS $$
 DECLARE
@@ -468,6 +387,8 @@ BEGIN
     SELECT
         u.hashed_id AS hashed_id_doctor,
         (ui.first_name || ' ' || ui.last_name)::VARCHAR(255) AS doctor_name,
+        u.email AS email,
+        ui.phone_number AS phone_number,
         hud.created_at AS link_date
     FROM
         health_unit_doctor hud
@@ -617,6 +538,8 @@ CREATE OR REPLACE FUNCTION get_health_unit_patients(
 RETURNS TABLE(
     hashed_id_patient VARCHAR(255), 
     patient_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(255),
     link_date TIMESTAMP
 ) AS $$
 DECLARE
@@ -645,6 +568,8 @@ BEGIN
     SELECT
         u.hashed_id AS hashed_id_patient,
         (ui.first_name || ' ' || ui.last_name)::VARCHAR(255) AS patient_name,
+        u.email AS email,
+        ui.phone_number AS phone_number,
         hup.created_at AS link_date
     FROM
         health_unit_patient hup
