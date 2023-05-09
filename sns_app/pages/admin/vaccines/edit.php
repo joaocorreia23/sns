5e9e14cc7fa3cdef2cbd93b2498f878dd0111b0f
Rui Cruz <ruicrux@hotmail.com>
2023-05-09 23:33:02 +0100

    Updates Rui+Thays
    -Vaccines List
    -Vaccines Add
    -Vaccines Edit

--- sns_app/pages/admin/vaccines/edit.php
+++ sns_app/pages/admin/vaccines/edit.php
@@ -1,5 +1,20 @@
 <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/head.php") ?>
-<?php $page_name = "Adicionar Vacina" ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/api/api.php") ?>
+
+
+<?php
+$id = isset($_GET["id"]) && !empty($_GET["id"]) ? $_GET["id"] : null;
+$api = new Api();
+$vaccine_info = $api->fetch("vaccines/", null, $id);
+if($vaccine_info['status']){
+    $vaccine_info = $vaccine_info['response']['data'];
+}else{
+    header("Location: list");
+    exit();
+}
+?>
+
+<?php $page_name = "Editar Vacina - " . $vaccine_info['vaccine_name'] ?>
 
 <body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" class="app-default">
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
@@ -19,7 +34,7 @@
                                     <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_profile_details" aria-expanded="true" aria-controls="kt_account_profile_details">
                                         <!--begin::Card title-->
                                         <div class="card-title m-0">
-                                            <h3 class="fw-bold m-0">Adicionar Nova Vacina</h3>
+                                            <h3 class="fw-bold m-0">Editar Vacina - <?php echo $vaccine_info['vaccine_name'] ?></h3>
                                         </div>
                                         <!--end::Card title-->
                                     </div>
@@ -27,15 +42,14 @@
                                     <!--begin::Content-->
                                     <div id="kt_account_settings_profile_details" class="collapse show">
                                         <!--begin::Form-->
-                                        <form id="form-add-vaccine" class="form fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate">
+                                        <form id="form-edit-vaccine" class="form fv-plugins-bootstrap5 fv-plugins-framework" novalidate="novalidate">
                                             <!--begin::Card body-->
                                             <div class="card-body border-top p-9">
-
                                                 <div class="row mb-6">
                                                     <div class="col-12">
                                                         <label class="col-lg-12 col-form-label required fw-semibold fs-6">Nome da Vacina</label>
                                                         <div class="col-lg-12 fv-row fv-plugins-icon-container">
-                                                            <input type="text" name="vaccine_name" class="form-control form-control-lg form-control-solid" placeholder="Nome da Vacina" value="">
+                                                            <input type="text" name="vaccine_name" class="form-control form-control-lg form-control-solid" placeholder="Nome da Vacina" value="<?php echo $vaccine_info['vaccine_name'] ?>">
                                                             <div class="fv-plugins-message-container invalid-feedback"></div>
                                                         </div>
                                                     </div>
@@ -70,7 +84,7 @@
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-            const form = document.getElementById("form-add-vaccine");
+            const form = document.getElementById("form-edit-vaccine");
             form.addEventListener("submit", insertHealthUnit);
 
             const api_url = "http://localhost:3000/api/";
@@ -79,15 +93,16 @@
             function insertHealthUnit() {
                 event.preventDefault();
 
-                var form = document.getElementById("form-add-vaccine");
+                var form = document.getElementById("form-edit-vaccine");
 
                 const formData = {
+                    hashed_id: '<?php echo $id ?>',
                     vaccine_name: form.vaccine_name.value
                 };
 
 
-                fetch(api_url + path + "insert", {
-                        method: "POST",
+                fetch(api_url + path + "update", {
+                        method: "PUT",
                         headers: {
                             "Content-Type": "application/json",
                         },
