a0f1d0c97c31008db24ed4d93f6e6cb0cb06d4f0
João Correia <j.correia2213@gmail.com>
2023-05-09 22:24:21 +0100

    API New Features e APP Health Unit All
    API Features e APP Health Unit All

---
+++ sns_app/pages/admin/vaccines/list.php
@@ -0,0 +1,31 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/head.php") ?>
+
+<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true" class="app-default">
+	<div class="d-flex flex-column flex-root app-root" id="kt_app_root">
+		<div class="app-page flex-column flex-column-fluid" id="kt_app_page">
+		<?php require_once($_SERVER["DOCUMENT_ROOT"]."/header.php") ?>
+			<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+			<?php require_once($_SERVER["DOCUMENT_ROOT"]."/toolbar.php") ?>
+				<div class="app-container container-xxl">
+					<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+						<div class="d-flex flex-column flex-column-fluid">
+							<div id="kt_app_content" class="app-content">
+							
+							
+							<!-- Content Here -->
+
+
+
+							</div>
+						</div>
+						<?php require_once($_SERVER["DOCUMENT_ROOT"]."/footer.php") ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/foo.php") ?>
+
+</body>
+</html>
