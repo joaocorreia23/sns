ee4342e870475d2ef6bf75afe464b22597f2d5dd
Rui Cruz <ruicrux@hotmail.com>
2023-05-09 22:23:31 +0100

    DB structure

--- sns_bd/functions/health_unit/health_unit.sql
+++ sns_bd/functions/health_unit/health_unit.sql
@@ -1,3 +1,17 @@
+--Check Health Unit Type
+CREATE OR REPLACE FUNCTION check_valid_health_unit_type(
+    IN health_unit_type_name VARCHAR(255)
+) RETURNS BOOLEAN AS $$
+DECLARE
+  valid_health_unit_types text[];
+BEGIN
+  valid_health_unit_types := enum_range(NULL::health_unit_type);
+  RETURN health_unit_type_name = ANY(valid_health_unit_types);
+END;
+$$ LANGUAGE plpgsql;
+--
+--
+--
 -- Check Health Unit Tax Number
 CREATE OR REPLACE FUNCTION check_health_unit_tax_number(
     tax_number INT
@@ -45,7 +59,7 @@ CREATE OR REPLACE FUNCTION create_health_unit(
     name VARCHAR(255),
     phone_number VARCHAR(255) DEFAULT NULL,
     email VARCHAR(255) DEFAULT NULL,
-    type INTEGER DEFAULT NULL,
+    type health_unit_type DEFAULT NULL,
     tax_number INT DEFAULT NULL,
     door_number VARCHAR(255) DEFAULT NULL,
     floor VARCHAR(255) DEFAULT NULL,
@@ -68,6 +82,8 @@ BEGIN
 
     IF type IS NULL THEN
         RAISE EXCEPTION 'O tipo da unidade de saúde não pode ser nulo';
+    ELSIF NOT check_valid_health_unit_type(type::VARCHAR(255)) THEN
+        RAISE EXCEPTION 'O tipo da unidade de saúde não é válido';
     END IF;
 
     IF tax_number IS NULL THEN
@@ -118,6 +134,9 @@ $$ LANGUAGE plpgsql;
 --
 --
 --
+--
+--
+--
 -- Set Hashed ID on New Rows
 CREATE OR REPLACE FUNCTION set_health_unit_hashed_id()
 RETURNS TRIGGER AS $$
@@ -140,14 +159,15 @@ EXECUTE PROCEDURE set_health_unit_hashed_id();
 -- Get Health Unit
 CREATE OR REPLACE FUNCTION get_health_unit(
     IN id_health_unit_in BIGINT DEFAULT NULL,
-    IN hashed_id_in VARCHAR(255) DEFAULT NULL
+    IN hashed_id_in VARCHAR(255) DEFAULT NULL,
+    IN status_in INTEGER DEFAULT NULL
 )
 RETURNS TABLE (
     hashed_id VARCHAR(255),
     name VARCHAR(255),
     phone_number VARCHAR(255),
     email VARCHAR(255),
-    type INTEGER,
+    type health_unit_type,
     tax_number INT,
     id_address BIGINT,
     door_number VARCHAR(255),
@@ -158,9 +178,17 @@ RETURNS TABLE (
     district_name VARCHAR(255),
     id_country BIGINT,
     country_name VARCHAR(255),
+    status INTEGER,
     created_at TIMESTAMP
 ) AS $$
 BEGIN
+    IF status_in IS NULL THEN
+        status_in = 1;
+    ELSEIF status_in NOT IN (0, 1) THEN
+        RAISE EXCEPTION 'O estado da unidade de saúde tem de ser 0 ou 1';
+    END IF;
+
+
     IF hashed_id_in IS NULL AND id_health_unit_in IS NULL THEN
         RETURN QUERY SELECT 
             hu.hashed_id, hu.name, hu.phone_number, hu.email, hu.type, hu.tax_number, hu.id_address,
@@ -169,13 +197,14 @@ BEGIN
             c.county_name,
             d.district_name,
             cty.id_country, cty.country_name,
-            hu.created_at
+            hu.status, hu.created_at
         FROM health_unit hu
         LEFT JOIN address ad ON hu.id_address=ad.id_address
         LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
         LEFT JOIN county c ON zc.id_county=c.id_county
         LEFT JOIN district d ON c.id_district=d.id_district
-        LEFT JOIN country cty ON d.id_country=cty.id_country;
+        LEFT JOIN country cty ON d.id_country=cty.id_country
+        WHERE hu.status=status_in;
     ELSEIF hashed_id_in IS NULL THEN
         RETURN QUERY SELECT 
             hu.hashed_id, hu.name, hu.phone_number, hu.email, hu.type, hu.tax_number, hu.id_address,
@@ -184,14 +213,15 @@ BEGIN
             c.county_name,
             d.district_name,
             cty.id_country, cty.country_name,
-            hu.created_at
+            hu.status, hu.created_at
         FROM health_unit hu
         LEFT JOIN address ad ON hu.id_address=ad.id_address
         LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
         LEFT JOIN county c ON zc.id_county=c.id_county
         LEFT JOIN district d ON c.id_district=d.id_district
         LEFT JOIN country cty ON d.id_country=cty.id_country
-        WHERE hu.id_health_unit = id_health_unit_in;
+        WHERE hu.id_health_unit = id_health_unit_in
+        AND hu.status=status_in;
         IF NOT FOUND THEN
             RAISE EXCEPTION 'Unidade de Saúde com o id "%" não existe', id_health_unit_in; --USER NOT FOUND
         END IF;
@@ -203,18 +233,138 @@ BEGIN
             c.county_name,
             d.district_name,
             cty.id_country, cty.country_name,
-            hu.created_at
+            hu.status, hu.created_at
         FROM health_unit hu
         LEFT JOIN address ad ON hu.id_address=ad.id_address
         LEFT JOIN zip_code zc ON ad.id_zip_code=zc.id_zip_code
         LEFT JOIN county c ON zc.id_county=c.id_county
         LEFT JOIN district d ON c.id_district=d.id_district
         LEFT JOIN country cty ON d.id_country=cty.id_country
-        WHERE hu.hashed_id = hashed_id_in;
+        WHERE hu.hashed_id = hashed_id_in
+        AND hu.status=status_in;
         IF NOT FOUND THEN
             RAISE EXCEPTION 'Unidade de Saúde com o hashed_id "%" não existe', hashed_id_in; --USER NOT FOUND
         END IF;
     END IF;
+END;
+$$ LANGUAGE plpgsql;
+--
+--
+--
+CREATE OR REPLACE FUNCTION get_health_unit_types()
+RETURNS TABLE (unit_type health_unit_type)
+AS $$
+BEGIN
+    RETURN QUERY SELECT unnest(enum_range(NULL::health_unit_type));
+END;
+$$ LANGUAGE plpgsql;
+--
+--
+--
+-- Change Health Unit Status
+CREATE OR REPLACE FUNCTION change_health_unit_status(
+    IN id_health_unit_in BIGINT DEFAULT NULL,
+    IN hashed_id_in VARCHAR(255) DEFAULT NULL,
+    IN status_in INTEGER DEFAULT NULL
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+    IF status_in IS NULL THEN
+        RAISE EXCEPTION 'O estado da unidade de saúde tem de ser 0 ou 1';
+    ELSEIF status_in NOT IN (0, 1) THEN
+        RAISE EXCEPTION 'O estado da unidade de saúde tem de ser 0 ou 1';
+    END IF;
+
+    IF hashed_id_in IS NULL AND id_health_unit_in IS NULL THEN
+        RAISE EXCEPTION 'O id da unidade de saúde ou o hashed_id devem ser informados';
+    ELSEIF hashed_id_in IS NOT NULL AND id_health_unit_in IS NOT NULL THEN
+        RAISE EXCEPTION 'Apenas o id da unidade de saúde ou o hashed_id devem ser informados';
+    ELSEIF hashed_id_in IS NULL AND (SELECT status FROM health_unit WHERE id_health_unit=id_health_unit_in) = status_in THEN
+        IF status_in = 0 THEN
+            RAISE EXCEPTION 'A unidade de saúde já se encontra desativada';
+        ELSE
+            RAISE EXCEPTION 'A unidade de saúde já se encontra ativada';
+        END IF;
+    ELSEIF id_health_unit_in IS NULL AND (SELECT status FROM health_unit WHERE hashed_id=hashed_id_in) = status_in THEN
+        IF status_in = 0 THEN
+            RAISE EXCEPTION 'A unidade de saúde já se encontra desativada';
+        ELSE
+            RAISE EXCEPTION 'A unidade de saúde já se encontra ativada';
+        END IF;
+    END IF;
+
+    IF hashed_id_in IS NULL THEN
+        UPDATE health_unit SET status=status_in WHERE id_health_unit=id_health_unit_in;
+        IF NOT FOUND THEN
+            RAISE EXCEPTION 'Unidade de Saúde com o id "%" não existe', id_health_unit_in; --USER NOT FOUND
+        END IF;
+    ELSE
+        UPDATE health_unit SET status=status_in WHERE hashed_id=hashed_id_in;
+        IF NOT FOUND THEN
+            RAISE EXCEPTION 'Unidade de Saúde com o hashed_id "%" não existe', hashed_id_in; --USER NOT FOUND
+        END IF;
+    END IF;
+        
+    RETURN TRUE;
+END;
+--
+--
+--
+CREATE OR REPLACE FUNCTION get_health_unit_types()
+RETURNS TABLE (unit_type health_unit_type)
+AS $$
+BEGIN
+    RETURN QUERY SELECT unnest(enum_range(NULL::health_unit_type));
+END;
+$$ LANGUAGE plpgsql;
+--
+--
+--
+-- Change Health Unit Status
+CREATE OR REPLACE FUNCTION change_health_unit_status(
+    IN id_health_unit_in BIGINT DEFAULT NULL,
+    IN hashed_id_in VARCHAR(255) DEFAULT NULL,
+    IN status_in INTEGER DEFAULT NULL
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+    IF status_in IS NULL THEN
+        RAISE EXCEPTION 'O estado da unidade de saúde tem de ser 0 ou 1';
+    ELSEIF status_in NOT IN (0, 1) THEN
+        RAISE EXCEPTION 'O estado da unidade de saúde tem de ser 0 ou 1';
+    END IF;
+
+    IF hashed_id_in IS NULL AND id_health_unit_in IS NULL THEN
+        RAISE EXCEPTION 'O id da unidade de saúde ou o hashed_id devem ser informados';
+    ELSEIF hashed_id_in IS NOT NULL AND id_health_unit_in IS NOT NULL THEN
+        RAISE EXCEPTION 'Apenas o id da unidade de saúde ou o hashed_id devem ser informados';
+    ELSEIF hashed_id_in IS NULL AND (SELECT status FROM health_unit WHERE id_health_unit=id_health_unit_in) = status_in THEN
+        IF status_in = 0 THEN
+            RAISE EXCEPTION 'A unidade de saúde já se encontra desativada';
+        ELSE
+            RAISE EXCEPTION 'A unidade de saúde já se encontra ativada';
+        END IF;
+    ELSEIF id_health_unit_in IS NULL AND (SELECT status FROM health_unit WHERE hashed_id=hashed_id_in) = status_in THEN
+        IF status_in = 0 THEN
+            RAISE EXCEPTION 'A unidade de saúde já se encontra desativada';
+        ELSE
+            RAISE EXCEPTION 'A unidade de saúde já se encontra ativada';
+        END IF;
+    END IF;
+
+    IF hashed_id_in IS NULL THEN
+        UPDATE health_unit SET status=status_in WHERE id_health_unit=id_health_unit_in;
+        IF NOT FOUND THEN
+            RAISE EXCEPTION 'Unidade de Saúde com o id "%" não existe', id_health_unit_in; --USER NOT FOUND
+        END IF;
+    ELSE
+        UPDATE health_unit SET status=status_in WHERE hashed_id=hashed_id_in;
+        IF NOT FOUND THEN
+            RAISE EXCEPTION 'Unidade de Saúde com o hashed_id "%" não existe', hashed_id_in; --USER NOT FOUND
+        END IF;
+    END IF;
+        
+    RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
 --
@@ -461,6 +611,11 @@ BEGIN
         RAISE EXCEPTION 'O utente já está associado à unidade de saúde';
     END IF;
 
+    -- Check if health unit is a "Centro de Saúde"
+    IF (SELECT type FROM health_unit WHERE health_unit.id_health_unit = id_health_unit_out) != 'Centro de Saúde' THEN
+        RAISE EXCEPTION 'A unidade de saúde não é um Centro de Saúde, logo não pode ter utentes associados';
+    END IF;
+
     INSERT INTO health_unit_patient (id_health_unit, id_patient) VALUES (id_health_unit_out, id_patient_out);
     RETURN TRUE;
 END;
@@ -579,6 +734,9 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
+
+
+
 
 
 
