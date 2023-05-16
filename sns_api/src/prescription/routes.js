55804277f04ce8d9eaba14a0c5ed5a6df90db249
Rui Cruz <ruicrux@hotmail.com>
2023-05-16 20:44:53 +0100

    New Prescription Insert + List
    - Possible to insert the medications at the same time as the prescription
    - List now brings the medications associated

--- sns_api/src/prescription/routes.js
+++ sns_api/src/prescription/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.post('/', controller.Get_Prescriptions);
 router.post('/table', controller.Get_Prescriptions_DataTable);
 router.post('/insert', controller.Add_Prescription);
+router.post('/insert_new', controller.Add_Prescription_New);
 router.post('/update', controller.Update_Prescription);
 router.get('/:hashed_id', controller.Get_PrescriptionByHashedId);
 
